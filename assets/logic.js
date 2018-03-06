3dbd7ae5926bb3de7eca9aa828b9bfbf7df9411b
Nick Brown <n.brown.professional@gmail.com>
2018-03-06 12:30:47 -0700

    Commit Changes: Adds file structure to repository (see description)
    -Adds file structure
    --This commit adds in the index.html file in addition to the assets folder which contains
      the logic.js, style.css and bootstrap/jquery files.
    
    -Adds skeleton structure code to the HTML
    --Starting "required" code is added to HTML file including the links and tests for bootstrap, jquery, CSS, and JavaScript.

---
+++ assets/logic.js
@@ -0,0 +1,28 @@
+ $(document).ready(function(){
+
+    alert("test link");
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBxkkFYLEXpJWPfrwACgu2c1lT-CRiMsP4",
+    authDomain: "trainscheduler-aa6f3.firebaseapp.com",
+    databaseURL: "https://trainscheduler-aa6f3.firebaseio.com",
+    projectId: "trainscheduler-aa6f3",
+    storageBucket: "trainscheduler-aa6f3.appspot.com",
+    messagingSenderId: "190914099169"
+};
+
+
+firebase.initializeApp(config);
+
+
+
+
+
+
+
+
+
+
+
+ });
