d11002aa1cab2f34103af9e5a3177921b40e551d
Nick Brown <n.brown.professional@gmail.com>
2018-03-06 12:57:07 -0700

    Creates bootstrap html and adds comments in .js

--- assets/logic.js
+++ assets/logic.js
@@ -1,7 +1,5 @@
  $(document).ready(function(){
 
-    alert("test link");
-
   // Initialize Firebase
   var config = {
     apiKey: "AIzaSyBxkkFYLEXpJWPfrwACgu2c1lT-CRiMsP4",
@@ -14,6 +12,21 @@
 
 
 firebase.initializeApp(config);
+
+
+  //  When adding trains, administrators should be able to submit the following:
+
+  //  Train Name
+
+  //  Destination
+
+  //  First Train Time-- in military time
+
+  //  Frequency-- in minutes
+
+  //  Code this app to calculate when the next train will arrive; this should be relative to the current time.
+
+  //  Users from many different machines must be able to view same train times.
 
 
 
